5cbaf06d7d901f1c2d14a4c5c2679af9eff065df
kabunasra-cmyk <kabunasra@gmail.com>
2026-08-18 22:28:56 +0300

    Update app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -18,7 +18,7 @@ import { UsersModule } from './users/users.module';
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
       autoLoadEntities: true,
-      synchronize: false,
+      synchronize: true,
     }),
     UsersModule,
   ],
